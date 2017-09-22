33d8f2064719a0a86b4db6970c1cb1c1acc7a27d
Diwakar Kumar <kumardiwakar06@gmail.com>
2017-09-22 15:09:13 +0530

    First Commit

---
+++ TodoKids/lib/GoogleConstant.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleConstant.swift
+//  TodoKids
+//
+//  Created by Diwakar Kumar on 22/09/17.
+//  Copyright © 2017 SmartDeveloper. All rights reserved.
+//
+
+import Foundation
+enum GoogleKeys : String {
+    case apiKey = "AIzaSyAYnrr5b5riPxnRZlwCyQtkFRNL5j6BZSk"
+    //case serverKey = "AIzaSyABbFygJ6-81A-8O3FMa_gYjw3Uzlvy0YI"
+}
